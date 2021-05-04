4acef7fdbb6f6fc7af3294544562954bae8f8f30
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-05-04 13:24:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+cam=document.getElementById("cam");
+Webcam.attach("#cam");
+pr1="";
+pr2="";
+function snap(){
+    Webcam.snap(function(data_URI){
+        document.getElementById("snapshot").innerHTML='<img src="'+data_URI+'" id="res_img" class="img-responsive">';
+    })
+}
+classify=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/nqni5SxQj/model.json',modell);
+function modell(){
+    console.log("Wooooohooooo! Model Loaded")
+}
+function speak(){
+    var speakk=window.speechSynthesis;
+    predsp1="The first prediction is - "+pr1;
+    predsp2="The second prediction is - "+pr2;
+    pls_utter= new SpeechSynthesisUtterance(predsp1+predsp2);
+    speakk.speak(pls_utter);
+}
+function check() {
+    img_saved=document.getElementById("res_img");
+    classify.classify(img_saved,gr);
+}
+function gr(error,result) {
+    if (error) {
+        console.error("errrrorrrr !!! Malfunctionnnnn");
+    }
+    else {
+        console.log(result);
+        document.getElementById("en1").innerHTML=result[0].label;
+        document.getElementById("en2").innerHTML=result[1].label;
+        pr1=result[0].label;
+        pr2=result[1].label;
+        speak()
+        if(pr1=="Thumbsup"){
+            document.getElementById("upd-emg1").innerHTML="👍";
+        }
+        else if(pr1=="Yo"){
+            document.getElementById("upd-emg1").innerHTML="🤟";
+        }
+        else if(pr1=="Hi"){
+            document.getElementById("upd-emg1").innerHTML="✋";
+        }
+        else if (pr1=="Nothing"){
+            document.getElementById("upd-emg1").innerHTML="no gesture found try a new gesture or pls get out of the frame "
+        }
+        if(pr2=="Thumbsup"){
+            document.getElementById("upd-emg2").innerHTML="👍";
+        }
+        else if(pr2=="Yo"){
+            document.getElementById("upd-emg2").innerHTML="🤟";
+        }
+        else if(pr2=="Hi"){
+            document.getElementById("upd-emg2").innerHTML="✋";
+        }
+        else if (pr1=="Nothing"){
+            document.getElementById("upd-emg2").innerHTML="no gesture found try a new gesture or pls get out of the frame "
+        }
+    }
+}
+
